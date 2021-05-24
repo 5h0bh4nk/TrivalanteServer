b5817670d130b4bcd0e9b5bc15e4a15d215e5003
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-05-25 01:27:17 +0530

    Facebook-Passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey':'12345-67890-12345-12345',
-    'mongoUrl': 'mongodb://localhost:27017/Trivalent'
+    'mongoUrl': 'mongodb://localhost:27017/Trivalent',
+    'facebook': {
+        clientId: '2684658391827798',
+        clientSecret: '0c8ea9739774a2723a1c260a5d932f'
+    }
 }
